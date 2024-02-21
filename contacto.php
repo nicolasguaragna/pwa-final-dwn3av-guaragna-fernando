501f20116128b6d3d4d5294dadf04ff6fb15c9b2
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-02-21 20:17:45 -0300

    update

--- contacto.php
+++ contacto.php
@@ -1,38 +1,37 @@
 <?php
+//configuracion para permitir solicitudes desde cualquier origen
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {//verifica si la solicitud es POST
     // obtener datos del formulario
     $nombre = $_POST['nombre'] ?? '';
     $apellido = $_POST['apellido'] ?? '';
     $email = $_POST['email'] ?? '';
     $mensaje = $_POST['mensaje'] ?? '';
 
-    // validar datos
+    //para validar que todos los campos del form esten completos
     if (empty($nombre) || empty($apellido) || empty($email) || empty($mensaje)) {
-        http_response_code(400);
+        http_response_code(400);// para establecer codigo de respuesta
         echo json_encode(['error' => 'Todos los campos son obligatorios.']);
         exit();
     }
 
-    // validar formato de correo electrónico
+    //para validar formato de correo electrónico
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         http_response_code(400);
         echo json_encode(['error' => 'Formato de correo electrónico no válido']);
         exit();
     }
 
-    // validar longitud máxima del mensaje
+    //para validar longitud máxima del mensaje
     $maxLongitudMensaje = 500; // define el límite según las necesidades
     if (strlen($mensaje) > $maxLongitudMensaje) {
         http_response_code(400);
         echo json_encode(['error' => 'El mensaje excede la longitud máxima permitida']);
         exit();
     }
-
-    // realizar cualquier operación necesaria con los datos (almacenamiento, envío de correo, etc.)
 
     // devuelve una respuesta exitosa
     $response = ['success' => true, 'message' => 'Mensaje recibido con éxito', 'data' => $_POST];
